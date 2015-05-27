cc0cbdc673468b9197699a836afe3bf9f5b82414
Felix <ycangus@163.com>
2015-05-27 14:45:52 +0800

    this is first commit to create Pae

---
+++ bin/common/featureExt.py
@@ -0,0 +1,20 @@
+#/usr/bin/python
+
+import os
+import sys
+
+class featureExt:
+
+    def __init__(self):
+        pass
+
+
+    def init(self):
+        return True
+
+    def extract(self):
+        return True
+
+
+if __name__ ==  '__main__':
+    pass
---
+++ bin/common/tagger.py
@@ -0,0 +1,23 @@
+#/usr/bin
+
+import sys
+import os
+
+
+class tagger:
+
+
+    def __init__(self):
+        pass
+
+
+    def init(self):
+        return True
+
+
+    def tag(self):
+        return True
+
+
+if __name__ == '__main__':
+    pass
