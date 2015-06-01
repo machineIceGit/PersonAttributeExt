2aa7cc1652c2203cda033df7fbe720e73503b48b
Felix <ycangus@163.com>
2015-06-01 16:22:26 +0800

    all

--- bin/PaeManager.py
+++ bin/PaeManager.py
@@ -5,6 +5,7 @@ import os
 import sys
 import yaml
 from pipline.Record import Record
+import argparse
 
 class PaeManager:
     
@@ -36,12 +37,15 @@ class PaeManager:
                 if obj.init(paramDict):
                     print obj.__class__.__name__ + " init SUCCESS"
                 self._filters.append(obj)
+        fp.close()
         return True
 
     def run(self, record={}):
         
         # Extractors
-        
+
+        print "Load this Module Sucess"
+
         for obj in self._extractors:
             if obj.run(record):
                 print obj.__class__.__name__ + " run SUCCESS"
@@ -53,12 +57,34 @@ class PaeManager:
             if obj.run(record):
                 print obj.__class__.__name__ + " run SUCCESS"
         print record
+        return record
+
 
 if __name__ == '__main__':
 
+    argParse = argparse.ArgumentParser(usage='PaeManager', description='user input args')
+    argParse.add_argument('-i' ,'--inputFile', help ='input  file')
+    argParse.add_argument('-c','--confYaml', default='../conf/paeConf.yaml', help ='model conf yaml file')
+    argParse.add_argument('-t' ,'--targetName', help =' main person')
+
+    args = argParse.parse_args()
+    confYamlFile =args.confYaml 
     obj = PaeManager()
-    obj.init("../conf/paeConf.yaml")
-    record =Record({'TEXT':'邓琳凤在安徽日本早稻田大学也开始体操训练', 'TARGET_NAME':'邓枭枭'})
-    record =Record({'TEXT':'1998年，邓杰古一家迁往阜阳市区，邓琳凤的哥哥邓枭枭被体操教练郭少华选中练习体操，受哥哥影响，邓琳凤在安徽日本早稻田大学也开始体操训练，同年就读北京体校一年级。', 'TARGET_NAME':'邓枭枭'})
-    #record = Record({'TEXT':'张艺谋1945年毕业于广州外贸大学','TARGET_NAME':'张艺谋'})
-    obj.run(record)
+    obj.init(confYamlFile)
+
+    iptFile = args.inputFile
+    targetName = args.targetName
+    
+    fp = open('_tmp', 'w')
+
+    for line in open(iptFile):
+        line = line.strip().decode('utf-8')
+        record = Record({'TEXT': line, 'TARGET_NAME': targetName})
+        #record =Record({'TEXT':'邓琳凤在安徽日本早稻田大学也开始体操训练', 'TARGET_NAME':'邓枭枭'})
+        #record =Record({'TEXT':'1998年，邓杰古一家迁往阜阳市区，邓琳凤的哥哥邓枭枭被体操教练郭少华选中练习体操，受哥哥影响，邓琳凤在安徽日本早稻田大学也开始体操训练，同年就读北京体校一年级。', 'TARGET_NAME':'邓枭枭'})
+        #record =Record({'TEXT':'邓枭枭，女，是一名中国的演员。邓琳凤的哥哥邓枭枭被体操教练郭少华选中练习体操，受哥哥影响，邓琳凤在安徽日本早稻田大学也开始体操训练，同年就读北京体校一年级。', 'TARGET_NAME':'邓枭枭'})
+        #record = Record({'TEXT':'张艺谋1945年毕业于广州外贸大学','TARGET_NAME':'张艺谋'})
+
+        print >> fp , obj.run(record)
+    
+    fp.close()
--- bin/modelTrain.sh
+++ bin/modelTrain.sh
@@ -1,6 +1,6 @@
 python ModelTrain/CrfModelTrain.py -i $2 -c ../conf/ModelTrain/$1Conf.yaml > train_data/$1_train.data
 
 
-sudo ./../depence/CRF++-0.53/crf_learn -f 3 -c 4.0 template train/data/$1_train.data ../data/models/model_$1_v1
+sudo ./../depence/CRF++-0.53/crf_learn -f 3 -c 4.0 template train_data/$1_train.data ../data/models/model_$1
 
 
